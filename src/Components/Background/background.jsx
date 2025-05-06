7f8ed920c2edb789e57fd407939ee06f9d5c9e3c
Lorenzo Wyman <lwyman2704@gmail.com>
2025-05-06 19:19:34 -0400

    adjusting stuff

--- src/Components/Background/background.jsx
+++ src/Components/Background/background.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import './background.css' 
 import video1 from '../../assets/bckvideoreduced.mp4'
 import image1 from '../../assets/bckgrndimg.png'
-import image2 from '../../assets/zotonamelogo.png'
+import image2 from '../../assets/3rdphoto.jpg'
 
 
 const Background = ({playStatus, heroCount}) => {
@@ -15,10 +15,10 @@ const Background = ({playStatus, heroCount}) => {
     )
     } 
     else if (heroCount === 0){
-        return <img src={image1} className='background fade-in' alt="" />
+        return <img src={image2} className='background fade-in' alt="" />
     }
     else if (heroCount === 1){
-        return <img src={image2} className='background fade-in' alt="" />
+        return <img src={image1} className='background fade-in' alt="" />
     }
   
     return null;
